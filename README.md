8060546684e1545587b44d19486424e79a74a568
Leandro Louback <leandro.louback@mirumagency.com>
2022-10-04 09:25:37 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Rock Paper Scissors Game
+
+![preview](./.github/preview.png)
+
+## Tecnologies
+
+- HTML
+- CSS
+- Javascript
+- Git e Github
+
+[Link pro projeto](https://leandrolouback.github.io/rock-paper-scissors)
---
+++ script.js
@@ -0,0 +1,66 @@
+const totalScore = {computerScore: 0, playerScore: 0}
+
+function getComputerChoice() {
+  const rpsChoice = ['Rock', 'Paper', 'Scissors']
+  const randomNumber = Math.floor(Math.random() * 3)
+  return rpsChoice[randomNumber]
+}
+
+function getResult(playerChoice, computerChoice) {
+  let score;
+  if (playerChoice == computerChoice) {
+    score = 0
+  } else if (playerChoice == 'Rock' && computerChoice == 'Scissors') {
+    score = 1
+  } else if (playerChoice == 'Paper' && computerChoice == 'Rock') {
+    score = 1
+  } else if (playerChoice == 'Scissors' && computerChoice == 'Paper') {
+    score = 1
+  } else {
+    score = -1
+  }
+  return score
+}
+
+function showResult(score, playerChoice, computerChoice) {
+  const resultDiv = document.getElementById('result')
+  const handsDiv = document.getElementById('hands')
+  const playerScoreDiv = document.getElementById('player-score')
+  if (score == -1) {
+    resultDiv.innerText = 'You Lose!'
+  } else if (score == 0) {
+    resultDiv.innerText = "It's a tie"
+  } else {
+    resultDiv.innerText = 'You Won!'
+  }
+  handsDiv.innerText = `👱= ${playerChoice} vs 🤖= ${computerChoice}`
+  playerScoreDiv.innerText = `Score: ${totalScore['playerScore']}`
+}
+
+function onClickRPS(playerChoice) {
+  const computerChoice = getComputerChoice()
+  const score = getResult(playerChoice, computerChoice)
+  totalScore['playerScore'] += score;
+  showResult(score, playerChoice, computerChoice)
+}
+
+function playGame() {
+  const rpsButtons = document.querySelectorAll('.rpsButton')
+  rpsButtons.forEach(rpsButton => {
+    rpsButton.onclick = () => onClickRPS(rpsButton.value)
+  })
+  const endGameButton = document.getElementById('endGameButton')
+  endGameButton.onclick = () => endGame()
+}
+
+function endGame() {
+  let resultDiv = document.getElementById('result')
+  let handsDiv = document.getElementById('hands')
+  let playerScoreDiv = document.getElementById('player-score')
+  resultDiv.innerText = ''
+  handsDiv.innerText = ''
+  playerScoreDiv.innerText = ''
+}
+
+playGame()
+
